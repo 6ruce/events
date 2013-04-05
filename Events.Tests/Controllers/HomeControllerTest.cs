ee957b011e6808b16ffa4666221239f6e93515a3
bruce <i6ruce@gmail.com>
2013-04-05 23:45:10 +0300

    Remake project from VS

---
+++ Events.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using NUnit.Framework;
+using Events;
+using Events.Controllers;
+
+namespace Events.Tests.Controllers
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Index()
+        {
+            // Упорядочение
+            HomeController controller = new HomeController();
+
+            // Действие
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Утверждение
+            Assert.AreEqual("Измените этот шаблон, чтобы быстро приступить к работе над приложением ASP.NET MVC.", result.ViewBag.Message);
+        }
+
+        [Test]
+        public void About()
+        {
+            // Упорядочение
+            HomeController controller = new HomeController();
+
+            // Действие
+            ViewResult result = controller.About() as ViewResult;
+
+            // Утверждение
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void Contact()
+        {
+            // Упорядочение
+            HomeController controller = new HomeController();
+
+            // Действие
+            ViewResult result = controller.Contact() as ViewResult;
+
+            // Утверждение
+            Assert.IsNotNull(result);
+        }
+    }
+}

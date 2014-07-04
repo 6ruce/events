5e3af286f9ee99fb269d6a8c56eb9148b5ebe1e3
bruce <i6ruce@gmail.com>
2014-07-04 10:57:35 +0300

    Add client validation for event creation form.

--- Events.Front/Scripts/Translations.ts
+++ Events.Front/Scripts/Translations.ts
@@ -1,7 +1,25 @@
 ﻿module _$ {
+    // [ General ]
     export var ItemNumberSign = '#';
     export var TableColumn = {
         Name: 'Name',
         Description: 'Description',
     };
+    export var Button = {
+        Save : 'Save',
+        Cancel : 'Cancel'
+    };
+    // [/ General ]
+    
+    // [ Events ]
+    export var Events = {
+        Form: {
+            ValidationError: {
+                EmptyDescription : "Description to your event can not be empty.",
+                EmptyName : "Name your event."
+            }
+        }
+    };
+    // [/ Events ]
+
 }
